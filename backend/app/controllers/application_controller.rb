902f930b0374e52c6c4658f6aea7a8ae9b74081e
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-01-27 00:17:29 -0500

    session

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
         p "THIS IS SESSION #{session}"
         !!session[:user_id]
     end
-    def current_user
+    def current_user  
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     def authorized_user?
