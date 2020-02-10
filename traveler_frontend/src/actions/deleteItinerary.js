f9873bf8e3490199ac202b6cba3487562f1c29cb
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-02-09 23:38:27 -0500

    fixed nav issue

--- traveler_frontend/src/actions/deleteItinerary.js
+++ traveler_frontend/src/actions/deleteItinerary.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import history from '../history.js'
 import toggleForm from './toggleForm.js'
+import selectItinerary from './selectItinerary.js'
 import removeItinerary from './removeItinerary.js'
 
 export default function deleteItinerary(itineraryID) {
@@ -9,9 +10,10 @@ export default function deleteItinerary(itineraryID) {
             const result = await axios.delete(`http://localhost:3001/itineraries/${itineraryID}`);  
             const {data} = result;  
             if(data.status){
-                dispatch(toggleForm(''));
-                dispatch(removeItinerary(itineraryID));
                 history.push('/');
+                dispatch(toggleForm(''));
+                dispatch(selectItinerary(''));
+                dispatch(removeItinerary(itineraryID));
             } else {               
                 console.log('Failed To Delete')
             }  
--- traveler_frontend/src/components/Nav/Nav.js
+++ traveler_frontend/src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import toggleDropdown from '../../actions/toggleDropdown.js'
 import Dropdown from '../Dropdown/Dropdown.js'
