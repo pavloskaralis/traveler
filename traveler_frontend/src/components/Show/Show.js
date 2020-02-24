531c8d90eda7ef0e1267c6dcdd1858f4a126fc31
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-02-24 14:26:15 -0500

    show

--- traveler_frontend/src/components/Show/Show.js
+++ traveler_frontend/src/components/Show/Show.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import { connect } from 'react-redux'
-import './Show.css'
+import styled from 'styled-components'
+import rowIcon from '../../images/row.png'
 import getItinerary from '../../actions/getItinerary.js'
 import postPlanningRow from '../../actions/postPlanningRow.js'
 import postSchedulingRow from '../../actions/postSchedulingRow.js'
@@ -9,6 +10,101 @@ import Tools from '../Tools/Tools.js'
 import Row from '../Row/Row.js'
 import Form from '../Form/Form.js'
 
+const Wrapper = styled.div`
+    width: 100%;
+    height: calc(100vh - 60px);
+    display: flex;
+    flex-direction: column;
+    background-color: ${props => props.theme.cream}; 
+`;
+
+const Table = styled.div`
+    margin:0 auto; 
+    max-width: 1200px;
+    min-width: 500px; 
+    width: 100%; 
+    overflow: auto;
+    height: calc(100% - 120px); 
+    background-color: white; 
+    display: table-row-group;
+    box-sizing: border-box;
+    box-shadow: 0 0 4px 0 rgba(0,0,0,.5);
+`;
+
+const Head = styled.div`
+    max-width: 1200px;
+    min-width: 768px;
+    box-sizing: border-box;
+    min-height: 60px;
+    display: flex;
+    padding: 0 8px;
+    background-color: ${props => props.theme.black};
+    text-transform: capitalize;
+`;
+
+const Body = styled.div`
+    max-width: 1200px;
+    min-width: 768px;
+    box-sizing: border-box;
+    min-height: calc(100% - 120px);
+    height: calc(100% - 120px);
+    overflow-y: auto;
+    overflow-x: hidden;
+`;
+
+const TH = styled.div`
+    cursor: default;
+    color: ${props => props.orange ? props.theme.orange : 'white'}; 
+    font-weight: 600;
+    box-sizing: border-box;
+    min-height: 60px;
+    width: 16.67%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    font-family: Verdana;
+`;
+
+const AddRow = styled.div`
+    position: fixed;
+    align-self: auto;
+    min-width: 500px;
+    max-width: 1200px;
+    width: 100%;
+    min-height: 60px; 
+    justify-self: flex-end;
+    min-height: 60px;
+    background-image: url(${rowIcon});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 35px;
+    background-color: ${props => props.theme.black};
+    overflow: hidden;
+    cursor: pointer;
+
+    &:hover {
+        background-color: ${props => props.theme.blackHover};
+    }
+`;
+
+const Empty = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center; 
+
+    & div {
+        position: fixed;
+        width: 100%;
+        text-align: center;
+        color: ${props => props.theme.gray};
+        font-family: Verdana;
+        font-weight: 600;
+        max-width: 1200px;
+    }
+`;
 
 const mapStateToProps = state => {
     return {
@@ -27,7 +123,7 @@ const mapDispatchToProps = {
     toggleDropdown
 }
 
-function Show({dropdown, toggleDropdown, form, getItinerary, postPlanningRow, postSchedulingRow, userID, itinerary, table}) {
+let Show = ({dropdown, toggleDropdown, form, getItinerary, postPlanningRow, postSchedulingRow, userID, itinerary, table}) => {
     //pass userID to ensure user is associated with itineraryID in url param
     useEffect(()=> {
         getItinerary(userID);
@@ -37,19 +133,19 @@ function Show({dropdown, toggleDropdown, form, getItinerary, postPlanningRow, po
     const autoScroll = () => setTimeout(()=>document.querySelector('.body').scrollTop = document.querySelector('.body').scrollHeight, 100);
     return (
         // dropdown off focus click
-        <div className='show-container' onClick={()=> {if(dropdown)toggleDropdown(false)}}>
+        <Wrapper onClick={()=> {if(dropdown)toggleDropdown(false)}}>
             {form && <Form page='show'/>}
             <Tools page='show'/>
-                <div className='table'>
-                <div className='head'>
-                    <div className='th' id='orange'>{table === 'Planning' ? 'Activity' : 'Time'}</div>
-                    <div className='th'>{table === 'Planning' ? 'Type' : 'Activity'}</div>
-                    <div className='th'>{table === 'Planning' ? 'Address' : 'Type'}</div>
-                    <div className='th'>{table === 'Planning' ? 'Website' : 'Address'}</div>
-                    <div className='th'>{table === 'Planning' ? 'Interest' : 'Website'}</div>
-                    <div className='th'>{table === 'Planning' ? 'Schedule' : 'Remove'}</div>
-                </div>
-                <div className='body'>
+            <Table>
+                <Head>
+                    <TH orange>{table === 'Planning' ? 'Activity' : 'Time'}</TH>
+                    <TH>{table === 'Planning' ? 'Type' : 'Activity'}</TH>
+                    <TH>{table === 'Planning' ? 'Address' : 'Type'}</TH>
+                    <TH>{table === 'Planning' ? 'Website' : 'Address'}</TH>
+                    <TH>{table === 'Planning' ? 'Interest' : 'Website'}</TH>
+                    <TH>{table === 'Planning' ? 'Schedule' : 'Remove'}</TH>
+                </Head>
+                <Body>
                     {/* render planning rows */}
                     {itinerary.planning_rows && table === 'Planning' && itinerary.planning_rows.sort((a,b) => a.id - b.id).map(planningRow => {
                         return (
@@ -59,9 +155,9 @@ function Show({dropdown, toggleDropdown, form, getItinerary, postPlanningRow, po
 
                     {/* empty schedulowing table messsage */}
                     { itinerary && table !== 'Planning' && itinerary.scheduling_rows.filter(row => row.date === table).length === 0 &&
-                        <div className='empty-container'>
-                            <div className='show-empty'>You Have Nothing Scheduled On This Date</div>
-                        </div>
+                        <Empty>
+                            <div>You Have Nothing Scheduled On This Date</div>
+                        </Empty>
                     }
                     
                     {/* render scheduling rows */}
@@ -71,11 +167,11 @@ function Show({dropdown, toggleDropdown, form, getItinerary, postPlanningRow, po
                             (table === schedulingRow.date) && <Row rowType='scheduling' row={schedulingRow} key={schedulingRow.id}/>
                         )
                     })}
-                </div>
+                </Body>
                 {/* create scheduling or planning row based on table */}
-                <div className='add-row' onClick={table !== 'Planning' ? ()=> postSchedulingRow(itinerary.id, table) : ()=> {postPlanningRow(itinerary.id); autoScroll();}}></div>
-            </div>
-        </div>
+                <AddRow onClick={table !== 'Planning' ? ()=> postSchedulingRow(itinerary.id, table) : ()=> {postPlanningRow(itinerary.id); autoScroll();}}/>
+            </Table>
+        </Wrapper>
     )
 }
 
